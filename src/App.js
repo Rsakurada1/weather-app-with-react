ccc120119265b7af8d69566983fa7dd5f2db08cd
Rikito Sakurada <rikito82k@gmail.com>
2023-09-08 16:22:45 +0900

    背景設定

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+const api = "https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=37a68e1a7debd2495d0e17b1d525cd13"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className="container">
+        <div className="top">
+        <div className="location">
+          <p>Tpkyo</p>
+        </div>
+        <div className="temp">
+          <h1>30°</h1>
+        </div>
+        <div className="description">
+        <p>Clouds</p>
+        </div>
+        </div>
+        <div className="bottom">
+        <div className="feel">
+          <p>35°</p>
+        </div>
+        <div className="humidity">
+          <p>20%</p>
+        </div>
+        <div className="wind">
+          12 MPH
+         </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
