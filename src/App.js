8c476fbbc643057f744e230eaf13396fdcb2d679
Rikito Sakurada <rikito82k@gmail.com>
2023-09-08 21:10:27 +0900

    初期値をtokyoに変更

--- src/App.js
+++ src/App.js
@@ -6,26 +6,33 @@ import axios from "axios";
 function App() {
 
   const [data, setData] = useState({});
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState('Tokyo');
   const [api, setApi] = useState('');
+
+  useEffect(() => {
+    fetchWeatherData('Tokyo');
+  }, []);
 
   useEffect(() => {
     setApi(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=37a68e1a7debd2495d0e17b1d525cd13`);
   }, [location]);
 
-  const searchLocation = (event) => {
-    if (event.key === 'Enter') {
-      axios.get(api)
-      .then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
+  const fetchWeatherData = (query) => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=37a68e1a7debd2495d0e17b1d525cd13`)
+    .then((response) => {
+      setData(response.data);
+    })
+    .catch((error) => {
       console.error('API request failed', error);
-      })
-      setLocation('')
+    });
+  } 
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter') {
+      fetchWeatherData(location);
+      setLocation('');
     }
-  }
+  };
 
   return (
     <div className='app'>
@@ -43,7 +50,7 @@ function App() {
           <p>{data.name}</p>
         </div>
         <div className="temp">
-          <h1>{Math.round(data.main.temp) - 273}℃</h1>
+        <h1>{data.main ? Math.round(data.main.temp - 273.15) : 'Loading....'}℃</h1>
         </div>
         <div className="description">
         <p>曇り</p>
