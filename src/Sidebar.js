3d581b3566ea497844772b93023bf96312873226
Rikito Sakurada <rikito82k@gmail.com>
2023-09-21 18:50:41 +0900

    サイドバーの修正

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Sidebar.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 function Sidebar({ open }) {
   return (
     <div className={`sidebar ${open ? 'open' : ''}`}>
-      <ul>
-        <li className='list'>ログイン</li>
-        <li className='list'>天気ニュース</li>
-        <li className='list'>お気に入りリスト</li>
-      </ul>
+        <button className='login-list'>Googleでログイン</button>
+        <button className='fav-list'>お気に入りリスト</button>  
     </div>
   );
 }
